7dc424922cdb3790e64baca6d5a5e106e51ce28b
Adrian Castillejos <zioyero@gmail.com>
2013-01-18 16:15:50 -0500

    Added new activity
    Added activity and modified the main menu to actually do things. Buttons
    still do not work.

--- src/zioyero/tvshows/MainMenu.java
+++ src/zioyero/tvshows/MainMenu.java
@@ -1,13 +1,10 @@
 package zioyero.tvshows;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.GridView;
-import android.widget.Toast;
 
 public class MainMenu extends Activity {
 
@@ -15,15 +12,6 @@ public class MainMenu extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_menu);
-		
-		GridView gridview = (GridView) findViewById(R.id.gridview);
-		gridview.setAdapter(new ImageAdapter(this));
-		
-		gridview.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-				Toast.makeText(MainMenu.this, "" + position, Toast.LENGTH_SHORT).show();
-			}
-		});
 	}
 		
 
@@ -32,6 +20,12 @@ public class MainMenu extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main_menu, menu);
 		return true;
+	}
+	
+	public void myShowsGo(View view)
+	{
+		Intent intent = new Intent(this, MyShowsActivity.class);
+		startActivity(intent);
 	}
 
 }
